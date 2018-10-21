d5a28770d63a676568985e07504462a3a0de7cfd
Steve Kemp <steve@steve.org.uk>
2018-10-21 08:04:00 +0300

    Minor comment updates

--- embed/main.go
+++ embed/main.go
@@ -1,3 +1,14 @@
+// main.go - Example of embedding the BASIC interpreter.
+//
+// This example demonstrates several things:
+//
+//  1. Setting a variable from golang which will be visible to BASIC.
+//
+//  2. Defining custom functions (PEEK, POKE, DOT, SAVE).
+//
+//  3. Retrieving the contents of BASIC values back to golang.
+//
+
 package main
 
 import (
@@ -16,13 +27,23 @@ import (
 	"github.com/skx/gobasic/tokenizer"
 )
 
-// Image
+// img holds a canvas.
+//
+// The DOT primitive allows setting a pixel, and this is the image upon
+// which it will be set.
 var img *image.RGBA
 
+// peekFunction is the golang implementation of the PEEK primitive,
+// which is made available to BASIC.
+// We just log that we've been invoked here.
 func peekFunction(env eval.Interpreter, args []token.Token) (float64, error) {
 	fmt.Printf("PEEK called with %v\n", args[0])
 	return 0, nil
 }
+
+// pokeFunction is the golang implementation of the PEEK primitive,
+// which is made available to BASIC.
+// We just log that we've been invoked here, along with the (three) args.
 func pokeFunction(env eval.Interpreter, args []token.Token) (float64, error) {
 	fmt.Printf("POKE called.\n")
 	for i, e := range args {
@@ -31,7 +52,10 @@ func pokeFunction(env eval.Interpreter, args []token.Token) (float64, error) {
 	return 0, nil
 }
 
-// Draw a DOT at a given X,Y coordinate
+// dotFunction is the golang implementation of the DOT primitive.
+//
+// It is invoked with three arguments (NUMBER COMMA NUMBER) and sets
+// the corresponding pixel in our canvas to be Red.
 func dotFunction(env eval.Interpreter, args []token.Token) (float64, error) {
 
 	x := 0
@@ -79,9 +103,19 @@ func dotFunction(env eval.Interpreter, args []token.Token) (float64, error) {
 	return 0, nil
 }
 
-// Save our generated image to out.png
+// saveFunction is the golang implementation of the SAVE primitive,
+// which is made available to BASIC.
+// We save the image-canvas to the file `out.png`.
 func saveFunction(env eval.Interpreter, args []token.Token) (float64, error) {
 
+	// If we have no image, create it.
+	if img == nil {
+		img = image.NewRGBA(image.Rect(0, 0, 600, 400))
+		black := color.RGBA{0, 0, 0, 255}
+		draw.Draw(img, img.Bounds(), &image.Uniform{black}, image.ZP, draw.Src)
+	}
+
+	// Now write out the image.
 	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
 	png.Encode(f, img)
@@ -91,10 +125,9 @@ func saveFunction(env eval.Interpreter, args []token.Token) (float64, error) {
 func main() {
 
 	//
-	// Ensure we seed a random-number source
+	// Ensure we seed our random-number source
 	//
-	// This is required such that RND() returns suitable
-	// values that change.
+	// This is required such that RND() returns suitable values.
 	//
 	rand.Seed(time.Now().UnixNano())
 
@@ -109,9 +142,9 @@ func main() {
  50 PRINT "I'M NOW CREATING AN IMAGE!!!!\n"
  55 REM 640 should be enough for anybody
  60 FOR I = 1 TO 640
- 70 let x = RND 600
- 80 let y = RND 400
- 90 DOT x, y
+ 70  LET x = RND 600
+ 80  LET y = RND 400
+ 90  DOT x, y
 100 NEXT I
 110 SAVE
 120 PRINT "OPEN 'out.png' TO VIEW YOUR IMAGE!\n"
