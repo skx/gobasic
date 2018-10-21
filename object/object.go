1d04e904d0a31d13c75140d2ca71829ef64b906b
Steve Kemp <steve@steve.org.uk>
2018-10-21 21:25:44 +0300

    Minor comment updates for godoc.org

--- object/object.go
+++ object/object.go
@@ -1,21 +1,27 @@
 // Package object contains code to store values passed too/from BASIC.
 //
-// Our values are either numbers (float64) or strings.
+// Go allows a rich number of types, but when interpreting BASIC only
+// two types are supported: Numbers and Strings.
+//
+// Numbers are stored as `float64`, to allow holding both integers and
+// floating-point numbers.  When it comes to output our interpreter will
+// round values that are int-like to avoid showing "3.0000" when "3"
+// would be sufficient.
 package object
 
 // Type describes the type of an object.
 type Type string
 
-// pre-defined constant ObjectType
+// These are our object-types.
 const (
 	NUMBER = "NUMBER"
 	STRING = "STRING"
 )
 
-// Object is the interface that all of our various objects must implmenet.
+// Object is the interface that our types must implement.
 type Object interface {
 
-	// Type returns the type of our objects contents.
+	// Type returns the type of the object.
 	Type() Type
 }
 
@@ -31,7 +37,7 @@ func (s *StringObject) Type() Type {
 	return STRING
 }
 
-// NumberObject holds a number (float64).
+// NumberObject holds a number.
 type NumberObject struct {
 
 	// Value is the value our object wraps.
