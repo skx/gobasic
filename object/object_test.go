97936bea3b0ef3ff3ae33f2a8d966479d575dbfd
Steve Kemp <steve.kemp@supermetrics.com>
2018-10-23 12:29:10 +0300

    Added an error-object.
    In addition to adding the error-object (and the tests), I've
    also added a simple constructor because that seems like it would
    be useful.
    
    TODO: Come back and repeat that for the string/number objects.

--- object/object_test.go
+++ object/object_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// Test we can create string/ints and that they have the correct types
+// Test we can create error/int/string and that they have the correct types
 func TestTypes(t *testing.T) {
 
 	v := StringObject{Value: "Steve"}
@@ -16,5 +16,39 @@ func TestTypes(t *testing.T) {
 	n := NumberObject{Value: math.Pi}
 	if n.Type() != NUMBER {
 		t.Errorf("Wrong type for Number")
+	}
+
+	e := ErrorObject{Value: "You fail!"}
+	if e.Type() != ERROR {
+		t.Errorf("Wrong type for Error")
+	}
+}
+
+func TestError(t *testing.T) {
+
+	a := Error("Test")
+	b := Error("Test %d", 3)
+	c := Error("Test %s", "me")
+
+	// Test types
+	if a.Type() != ERROR {
+		t.Errorf("Object has the wrong type!")
+	}
+	if b.Type() != ERROR {
+		t.Errorf("Object has the wrong type!")
+	}
+	if c.Type() != ERROR {
+		t.Errorf("Object has the wrong type!")
+	}
+
+	// Test values
+	if a.Value != "Test" {
+		t.Errorf("Wrong value for error-message")
+	}
+	if b.Value != "Test 3" {
+		t.Errorf("Wrong value for error-message")
+	}
+	if c.Value != "Test me" {
+		t.Errorf("Wrong value for error-message")
 	}
 }
