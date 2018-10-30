5f3c08b0dc692e860961bc0022cc025e8ce487aa
Steve Kemp <steve@steve.org.uk>
2018-10-30 19:08:34 +0200

    Trivial port to new layout

---
+++ builtin/maths.go
@@ -0,0 +1,220 @@
+// The builtin package provides the ability to register our built-in functions.
+//
+// maths.go implements our math-related primitives
+
+package builtin
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"github.com/skx/gobasic/object"
+)
+
+// init ensures that we've initialized our random-number state
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// ABS implements ABS
+func ABS(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	// If less than zero make it positive.
+	if i < 0 {
+		return &object.NumberObject{Value: -1 * i}
+	}
+
+	// Otherwise return as-is.
+	return &object.NumberObject{Value: i}
+}
+
+// ACS (arccosine)
+func ACS(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Acos(i)}
+}
+
+// ASN (arcsine)
+func ASN(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Asin(i)}
+}
+
+// ATN (arctan)
+func ATN(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Atan(i)}
+}
+
+// BIN converts a number from binary.
+func BIN(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	s := fmt.Sprintf("%d", int(i))
+
+	b, err := strconv.ParseInt(s, 2, 64)
+	if err != nil {
+		return object.Error("BIN:%s", err.Error())
+	}
+
+	return &object.NumberObject{Value: float64(b)}
+
+}
+
+// COS implements the COS function..
+func COS(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Cos(i)}
+}
+
+// EXP x=e^x EXP
+func EXP(env interface{}, args []object.Object) object.Object {
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Exp(i)}
+}
+
+// INT implements INT
+func INT(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	// Truncate.
+	return &object.NumberObject{Value: float64(int(i))}
+}
+
+// LN calculates logarithms to the base e - LN
+func LN(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Log(i)}
+}
+
+// PI returns the value of PI
+func PI(env interface{}, args []object.Object) object.Object {
+	return &object.NumberObject{Value: math.Pi}
+}
+
+// RND implements RND
+func RND(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	// Ensure it is valid.
+	if i < 1 {
+		return object.Error("Argument to RND must be >0")
+	}
+
+	// Return the random number
+	return &object.NumberObject{Value: float64(rand.Intn(int(i)))}
+}
+
+// SGN is the sign function (sometimes called signum).
+func SGN(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	if i < 0 {
+		return &object.NumberObject{Value: -1}
+	}
+	if i == 0 {
+		return &object.NumberObject{Value: 0}
+	}
+	return &object.NumberObject{Value: 1}
+
+}
+
+// SIN operats the sin function.
+func SIN(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Sin(i)}
+}
+
+// SQR implements square root.
+func SQR(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Sqrt(i)}
+}
+
+// TAN implements the tan function.
+func TAN(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	return &object.NumberObject{Value: math.Tan(i)}
+}
---
+++ builtin/string.go
@@ -0,0 +1,210 @@
+// The builtin package provides the ability to register our built-in functions.
+//
+// string.go implements our string-related primitives
+
+package builtin
+
+import (
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+
+	"github.com/skx/gobasic/object"
+)
+
+// CHR returns the character specified by the given ASCII code.
+func CHR(env interface{}, args []object.Object) object.Object {
+
+	// Get the (float) argument.
+	if args[0].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.NumberObject).Value
+
+	// Now
+	r := rune(i)
+
+	return &object.StringObject{Value: string(r)}
+}
+
+// CODE returns the integer value of the specified character.
+func CODE(env interface{}, args []object.Object) object.Object {
+
+	// Get the (string) argument.
+	if args[0].Type() != object.STRING {
+		return object.Error("Wrong type")
+	}
+	i := args[0].(*object.StringObject).Value
+
+	if len(i) > 0 {
+		s := i[0]
+		return &object.NumberObject{Value: float64(rune(s))}
+	}
+	return &object.NumberObject{Value: float64(0)}
+
+}
+
+// LEFT returns the N left-most characters of the string.
+func LEFT(env interface{}, args []object.Object) object.Object {
+
+	// Get the (string) argument.
+	if args[0].Type() != object.STRING {
+		return object.Error("Wrong type")
+	}
+	in := args[0].(*object.StringObject).Value
+
+	// Get the (float) argument.
+	if args[1].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	n := args[1].(*object.NumberObject).Value
+
+	if int(n) > len(in) {
+		n = float64(len(in))
+	}
+
+	left := in[0:int(n)]
+
+	return &object.StringObject{Value: left}
+}
+
+// LEN returns the length of the given string
+func LEN(env interface{}, args []object.Object) object.Object {
+
+	// Get the (string) argument.
+	if args[0].Type() != object.STRING {
+		return object.Error("Wrong type")
+	}
+	in := args[0].(*object.StringObject).Value
+
+	// We need to count in UTF-8 characters.
+	len := utf8.RuneCountInString(in)
+
+	return &object.NumberObject{Value: float64(len)}
+}
+
+// MID returns the N characters from the given offset
+func MID(env interface{}, args []object.Object) object.Object {
+
+	// Get the (string) argument.
+	if args[0].Type() != object.STRING {
+		return object.Error("Wrong type")
+	}
+	in := args[0].(*object.StringObject).Value
+
+	// Get the (float) argument.
+	if args[1].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	offset := args[1].(*object.NumberObject).Value
+
+	// Get the (float) argument.
+	if args[2].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	count := args[2].(*object.NumberObject).Value
+
+	// too far
+	if int(offset) > len(in) {
+		return &object.StringObject{Value: ""}
+	}
+
+	// get the string from the position
+	out := in[int(offset):]
+
+	// now cut, by length
+	if int(count) > len(out) {
+		count = float64(len(out))
+	}
+	out = out[:int(count)]
+	return &object.StringObject{Value: out}
+}
+
+// RIGHT returns the N right-most characters of the string.
+func RIGHT(env interface{}, args []object.Object) object.Object {
+
+	// Get the (string) argument.
+	if args[0].Type() != object.STRING {
+		return object.Error("Wrong type")
+	}
+	in := args[0].(*object.StringObject).Value
+
+	// Get the (float) argument.
+	if args[1].Type() != object.NUMBER {
+		return object.Error("Wrong type")
+	}
+	n := args[1].(*object.NumberObject).Value
+
+	if int(n) > len(in) {
+		n = float64(len(in))
+	}
+	right := in[len(in)-int(n):]
+
+	return &object.StringObject{Value: right}
+}
+
+// STR converts a number to a string
+func STR(env interface{}, args []object.Object) object.Object {
+
+	// Error?
+	if args[0].Type() == object.ERROR {
+		return args[0]
+	}
+
+	// Already a string?
+	if args[0].Type() == object.STRING {
+		return args[0]
+	}
+
+	// Get the value
+	var i float64
+	i = args[0].(*object.NumberObject).Value
+	s := ""
+
+	if i == float64(int(i)) {
+		s = fmt.Sprintf("%d", int(i))
+	} else {
+		s = fmt.Sprintf("%f", i)
+	}
+	return &object.StringObject{Value: s}
+}
+
+// TL returns a string, minus the first character.
+func TL(env interface{}, args []object.Object) object.Object {
+
+	// Get the (string) argument.
+	if args[0].Type() != object.STRING {
+		return object.Error("Wrong type")
+	}
+	in := args[0].(*object.StringObject).Value
+
+	if len(in) > 1 {
+		rest := in[1:]
+
+		return &object.StringObject{Value: rest}
+	}
+	return &object.StringObject{Value: ""}
+}
+
+// VAL converts a string to a number
+func VAL(env interface{}, args []object.Object) object.Object {
+
+	// Error?
+	if args[0].Type() == object.ERROR {
+		return args[0]
+	}
+
+	// Already a number?
+	if args[0].Type() == object.NUMBER {
+		return args[0]
+	}
+
+	// Get the value
+	s := args[0].(*object.StringObject).Value
+	b, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return object.Error("VAL: %s", err.Error())
+	}
+
+	return &object.NumberObject{Value: float64(b)}
+}
