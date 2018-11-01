ed50468b369edbb427e8d07d96f73563728af972
Steve Kemp <steve@steve.org.uk>
2018-11-01 19:18:47 +0200

    Added simple test-case for go-fuzz input

---
+++ eval/fuzz_test.go
@@ -0,0 +1,22 @@
+// vars_test.go - Simple test-cases for our variable-store
+
+package eval
+
+import (
+	"testing"
+)
+
+// TestFuzz is just a simple wrapper that pretends we cover the fuzzer.
+func TestFuzz(t *testing.T) {
+
+	var data []byte
+	data = []byte(`
+10 REM
+`)
+
+	out := Fuzz(data)
+	if out != 1 {
+		t.Errorf("We found an unexpected result in Fuzz!")
+	}
+
+}
