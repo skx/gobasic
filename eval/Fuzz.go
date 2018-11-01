4ee3debed55c82d429079f17d77b7b7b44c5eacf
Steve Kemp <steve@steve.org.uk>
2018-11-01 19:20:19 +0200

    Added comment, to satisfy golint

--- eval/Fuzz.go
+++ eval/Fuzz.go
@@ -2,6 +2,8 @@ package eval
 
 import "github.com/skx/gobasic/tokenizer"
 
+// Fuzz is the function that our fuzzer-application uses.
+// See `FUZZING.md` in our distribution for how to invoke it.
 func Fuzz(data []byte) int {
 
 	tokener := tokenizer.New(string(data))
