e07cba645ed64aaed5f3b9ffcad582e596a3bb58
Steve Kemp <steve.kemp@supermetrics.com>
2019-03-01 09:16:20 +0200

    Run linting-checks on commits

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-# init modules
+# Install the lint-tool
+go get -u golang.org/x/lint/golint
+
+# Init the modules
 go mod init
+
+# At this point failures cause aborts
+set -e
+
+# Run the linter
+echo "Running linter .."
+golint -set_exit_status ./...
+echo "Linter complete .."
 
 # Run golang tests
 go test ./...
-
