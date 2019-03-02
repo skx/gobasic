dc4e7fb3eb28c55a9d160e4f6049465581ab0593
Andy Williams <andy@andy.xyz>
2019-03-02 18:27:15 +0000

    Allow setting of the output Writer for PRINT and DUMP
    Requires a concrete Environment type for builtin.
    Env was not used for anything so this seemed safe...

--- builtin/misc.go
+++ builtin/misc.go
@@ -5,49 +5,66 @@
 package builtin
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/andydotxyz/gobasic/object"
 )
 
 // DUMP just displays the only argument it received.
-func DUMP(env interface{}, args []object.Object) object.Object {
+func DUMP(env Environment, args []object.Object) object.Object {
+	var out *bufio.Writer
+	if env == nil {
+		out = bufio.NewWriter(os.Stdout)
+	} else {
+		out = env.StdOutput()
+	}
 
 	// Get the (float) argument.
 	if args[0].Type() == object.NUMBER {
 		i := args[0].(*object.NumberObject).Value
-		fmt.Printf("NUMBER: %f\n", i)
+		out.WriteString(fmt.Sprintf("NUMBER: %f\n", i))
 	}
 	if args[0].Type() == object.STRING {
 		s := args[0].(*object.StringObject).Value
-		fmt.Printf("STRING: %s\n", s)
+		out.WriteString(fmt.Sprintf("STRING: %s\n", s))
 	}
 	if args[0].Type() == object.ERROR {
 		s := args[0].(*object.ErrorObject).Value
-		fmt.Printf("Error: %s\n", s)
+		out.WriteString(fmt.Sprintf("Error: %s\n", s))
 	}
+	out.Flush()
 
 	// Otherwise return as-is.
 	return &object.NumberObject{Value: 0}
 }
 
 // PRINT handles displaying strings, integers, and errors.
-func PRINT(env interface{}, args []object.Object) object.Object {
+func PRINT(env Environment, args []object.Object) object.Object {
+	var out *bufio.Writer
+	if env == nil {
+		out = bufio.NewWriter(os.Stdout)
+	} else {
+		out = env.StdOutput()
+	}
 	for _, ent := range args {
 		switch ent.Type() {
 		case object.NUMBER:
 			n := ent.(*object.NumberObject).Value
 			if n == float64(int(n)) {
-				fmt.Printf("%d", int(n))
+				out.WriteString(fmt.Sprintf("%d", int(n)))
 			} else {
-				fmt.Printf("%f", n)
+				out.WriteString(fmt.Sprintf("%f", n))
 			}
 		case object.STRING:
-			fmt.Printf("%s", ent.(*object.StringObject).Value)
+			out.WriteString(fmt.Sprintf("%s", ent.(*object.StringObject).Value))
 		case object.ERROR:
-			fmt.Printf("%s", ent.(*object.ErrorObject).Value)
+			out.WriteString(fmt.Sprintf("%s", ent.(*object.ErrorObject).Value))
 		}
 	}
+	out.WriteRune('\n')
+	out.Flush()
 
 	// Return the count of values we printed.
 	return &object.NumberObject{Value: float64(len(args))}
