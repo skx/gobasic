de9ad8e8a2041ce4c8393dd4928d3600e7266ee9
Steve Kemp <steve@steve.org.uk>
2019-03-16 14:39:37 +0200

    Run the `go vet` shadowed-variable check.

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -3,8 +3,9 @@
 # This will allow the linter to be installed.  All a mess.
 rm go.mod
 
-# Install the lint-tool
+# Install the lint-tool, and the shadow-tool
 go get -u golang.org/x/lint/golint
+go get -u golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
 
 # Init the modules
 go mod init
@@ -13,9 +14,14 @@ go mod init
 set -e
 
 # Run the linter
-echo "Running linter .."
+echo "Launching linter .."
 golint -set_exit_status ./...
-echo "Linter complete .."
+echo "Completed linter .."
+
+# Run the shadow-checker
+echo "Launching shadowed-variable check .."
+go vet -vettool=$(which shadow)
+echo "Completed shadowed-variable check .."
 
 # Run golang tests
 go test ./...
