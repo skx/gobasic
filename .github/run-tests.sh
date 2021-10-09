7f06bb311c6291ce26857b08898809ed68b82c83
Steve Kemp <steve@steve.org.uk>
2021-10-09 08:18:40 +0300

    Install the linting tools we use the new way
    Don't use "go get -u ..", instead use "go install .."

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Install tools to test our code-quality.
-go get -u golang.org/x/lint/golint
-go get -u golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
-go get -u honnef.co/go/tools/cmd/staticcheck
+# Install the tools we use to test our code-quality.
+#
+# Here we setup the tools to install only if the "CI" environmental variable
+# is not empty.  This is because locally I have them installed.
+#
+# NOTE: Github Actions always set CI=true
+#
+if [ ! -z "${CI}" ] ; then
+    go install golang.org/x/lint/golint@latest
+    go install golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow@latest
+    go install honnef.co/go/tools/cmd/staticcheck@latest
+fi
 
 # Run the static-check tool - we ignore errors in goserver/static.go
 t=$(mktemp)
